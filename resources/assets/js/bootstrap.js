30eb798c8067b8e3735cb418834e7872ca0a6391
Max <max@maxkorlaar.com>
2017-01-24 19:38:37 +0100

    add stuff

--- resources/assets/js/bootstrap.js
+++ resources/assets/js/bootstrap.js
@@ -8,8 +8,7 @@ window._ = require('lodash');
  */
 
 window.$ = window.jQuery = require('jquery');
-require('bootstrap-sass');
-
+require('materialize');
 /**
  * Vue is a modern JavaScript library for building interactive web interfaces
  * using reactive data binding and reusable components. Vue's API is clean
