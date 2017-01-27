72c23b36537053fc78a9bcb6efce1ad461d22e3a
Max <max@maxkorlaar.com>
2017-01-27 20:34:56 +0100

    Hm :thinking:

--- app/Http/Controllers/Auth/LoginController.php
+++ app/Http/Controllers/Auth/LoginController.php
@@ -1,43 +1,48 @@
 <?php
 
-namespace App\Http\Controllers\Auth;
+    namespace App\Http\Controllers\Auth;
 
-use App\Http\Controllers\Controller;
-use Illuminate\Foundation\Auth\AuthenticatesUsers;
-
-/**
- * Class LoginController
- *
- * @package App\Http\Controllers\Auth
- */
-class LoginController extends Controller
-{
-    /*
-    |--------------------------------------------------------------------------
-    | Login Controller
-    |--------------------------------------------------------------------------
-    |
-    | This controller handles authenticating users for the application and
-    | redirecting them to your home screen. The controller uses a trait
-    | to conveniently provide its functionality to your applications.
-    |
-    */
-
-    use AuthenticatesUsers;
+    use App\Http\Controllers\Controller;
+    use Illuminate\Foundation\Auth\AuthenticatesUsers;
 
     /**
-     * Where to redirect users after login.
+     * Class LoginController
      *
-     * @var string
+     * @package App\Http\Controllers\Auth
      */
-    protected $redirectTo = '/user/gallery';
+    class LoginController extends Controller {
+        /*
+        |--------------------------------------------------------------------------
+        | Login Controller
+        |--------------------------------------------------------------------------
+        |
+        | This controller handles authenticating users for the application and
+        | redirecting them to your home screen. The controller uses a trait
+        | to conveniently provide its functionality to your applications.
+        |
+        */
 
-    /**
-     * Create a new controller instance.
-     *
-     */
-    public function __construct()
-    {
-        $this->middleware('guest', ['except' => 'logout']);
+        use AuthenticatesUsers;
+
+        /**
+         * Where to redirect users after login.
+         *
+         * @var string
+         */
+        protected $redirectTo = '/user/gallery';
+
+        /**
+         * Create a new controller instance.
+         *
+         */
+        public function __construct() {
+            $this->middleware('guest', ['except' => 'logout']);
+        }
+
+        /**
+         * @return string
+         */
+        public function username() {
+            return 'username';
+        }
     }
-}
