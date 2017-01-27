4040c519289564cc58184c1eb78c0bb2bb21f86e
Max <max@maxkorlaar.com>
2017-01-27 23:24:27 +0100

    Oh right, fix test data

--- database/seeds/DatabaseSeeder.php
+++ database/seeds/DatabaseSeeder.php
@@ -14,6 +14,6 @@ use Illuminate\Database\Seeder;
      */
     public function run()
     {
-        // $this->call(UsersTableSeeder::class);
+        $this->call(UsersTableSeeder::class);
     }
 }
