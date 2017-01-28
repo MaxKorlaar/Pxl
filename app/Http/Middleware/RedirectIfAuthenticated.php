1ff6462b2b6a59af67a64372655f74f2a9598dbf
Max <max@maxkorlaar.com>
2017-01-28 15:48:18 +0100

    It's redirect, not location

--- app/Http/Middleware/RedirectIfAuthenticated.php
+++ app/Http/Middleware/RedirectIfAuthenticated.php
@@ -18,7 +18,7 @@
         public function handle($request, Closure $next, $guard = null) {
             if (Auth::guard($guard)->check()) {
                 if ($request->ajax()) {
-                    return response(['success' => true, 'info' => 'Already authenticated', 'location' => route('home')]);
+                    return response(['success' => true, 'info' => 'Already authenticated', 'redirect' => route('home')]);
                 }
                 return redirect(route('home')); // If the user is already logged in, redirect them back to the home page
             }
