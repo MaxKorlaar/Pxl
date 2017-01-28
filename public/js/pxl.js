67cb9742786bee84aff78f4ec3c626cf96fefea6
Max <max@maxkorlaar.com>
2017-01-28 13:35:50 +0100

    Wrong syntax

--- public/js/pxl.js
+++ public/js/pxl.js
@@ -1,9 +1,5 @@
 $(document).ready(function() {
     $(".button-collapse").sideNav();
-    $("a#logout_button").on('click', function(e) {
-        e.preventDefault();
-        $("#logout_form").submit();
-    })
 });
 /*!
  * Materialize v0.97.8 (http://materializecss.com)
--- resources/assets/js/pxl.js
+++ resources/assets/js/pxl.js
@@ -1,7 +1,3 @@
 $(document).ready(function() {
     $(".button-collapse").sideNav();
-    $("a#logout_button").on('click', function(e) {
-        e.preventDefault();
-        $("#logout_form").submit();
-    })
 });
