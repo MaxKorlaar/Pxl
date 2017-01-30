e3e7112d0dff649775a80a5412bd4c7cbf84de3d
Max <max@maxkorlaar.com>
2017-01-30 19:37:01 +0100

    Clean that up

--- app/Http/Controllers/Auth/LoginController.php
+++ app/Http/Controllers/Auth/LoginController.php
@@ -5,7 +5,6 @@
     use App\Http\Controllers\Controller;
     use Illuminate\Foundation\Auth\AuthenticatesUsers;
     use Illuminate\Http\Request;
-    use Illuminate\Session\TokenMismatchException;
 
     /**
      * Class LoginController
