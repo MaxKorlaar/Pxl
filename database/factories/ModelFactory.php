90b21010aa3336974d1792092ecaa38ac9567797
Max <max@maxkorlaar.com>
2017-01-31 18:53:04 +0100

    Update factory. Reset migrations & add fake data again

--- database/factories/ModelFactory.php
+++ database/factories/ModelFactory.php
@@ -1,27 +1,28 @@
 <?php
 
-/*
-|--------------------------------------------------------------------------
-| Model Factories
-|--------------------------------------------------------------------------
-|
-| Here you may define all of your model factories. Model factories give
-| you a convenient way to create models for testing and seeding your
-| database. Just tell the factory how a default model should look.
-|
-*/
+    /*
+    |--------------------------------------------------------------------------
+    | Model Factories
+    |--------------------------------------------------------------------------
+    |
+    | Here you may define all of your model factories. Model factories give
+    | you a convenient way to create models for testing and seeding your
+    | database. Just tell the factory how a default model should look.
+    |
+    */
 
-/** @var \Illuminate\Database\Eloquent\Factory $factory */
-$factory->define(App\User::class, function (Faker\Generator $faker) {
-    static $password;
+    /** @var \Illuminate\Database\Eloquent\Factory $factory */
+    $factory->define(App\User::class, function (Faker\Generator $faker) {
+        static $password;
 
-    return [
-        'username' => $faker->name,
-        'email' => $faker->unique()->safeEmail,
-        'password' => $password ?: $password = bcrypt('secret'),
-        '2fa_token' => null,
-        'last_ip' => $faker->ipv4,
-        'active' => $faker->boolean,
-        'remember_token' => str_random(10),
-    ];
-});
+        return [
+            'username'       => $faker->name,
+            'email'          => $faker->unique()->safeEmail,
+            'password'       => $password ?: $password = bcrypt('secret'),
+            '2fa_token'      => null,
+            'last_ip'        => $faker->ipv4,
+            'active'         => $faker->boolean,
+            'rank'           => $faker->boolean ? 'member' : 'admin',
+            'remember_token' => str_random(10),
+        ];
+    });
