e513eed1be50257799d944e210bd9f89e5a69880
Max <max@maxkorlaar.com>
2017-02-01 15:18:07 +0100

    make it better

--- resources/lang/en/auth.php
+++ resources/lang/en/auth.php
@@ -1,31 +1,35 @@
 <?php
 
-return [
+    return [
 
-    /*
-    |--------------------------------------------------------------------------
-    | Authentication Language Lines
-    |--------------------------------------------------------------------------
-    |
-    | The following language lines are used during authentication for various
-    | messages that we need to display to the user. You are free to modify
-    | these language lines according to your application's requirements.
-    |
-    */
-    'log_in' => 'Log in',
-    'username' => 'Username or email',
-    'password' => 'Password',
-    'forgot_password' => 'Forgot password?',
-    'remember_me' => 'Remember me',
-    'failed' => 'These credentials are invalid.',
-    'account_inactive' => 'This account is currently not enabled',
-    'throttle' => 'Too many login attempts. Please try again in :seconds seconds.',
-    'register' => [
-        'username' => 'Username',
-        'password' => 'Password',
-        'email' => 'Email address',
-        'confirm_email' => 'Confirm email address',
-        'register' => 'Register'
-    ]
+        /*
+        |--------------------------------------------------------------------------
+        | Authentication Language Lines
+        |--------------------------------------------------------------------------
+        |
+        | The following language lines are used during authentication for various
+        | messages that we need to display to the user. You are free to modify
+        | these language lines according to your application's requirements.
+        |
+        */
+        'log_in'           => 'Log in',
+        'username'         => 'Username or email',
+        'password'         => 'Password',
+        'forgot_password'  => 'Forgot password?',
+        'remember_me'      => 'Remember me',
+        'failed'           => 'These credentials are invalid.',
+        'account_inactive' => 'This account is currently not enabled',
+        'throttle'         => 'Too many login attempts. Please try again in :seconds seconds.',
+        'register'         => [
+            'username'         => 'Username',
+            'password'         => 'Password',
+            'email'            => 'Email address',
+            'confirm_email'    => 'Confirm email address',
+            'confirm_password' => 'Confirm password',
+            'password_help'    => 'Please use a strong, preferably random, password. Do not share this lightly, not even with other websites.
+            Getting compromised on one service could result in losing access to your accounts on others, so please consider your password as highly valuable.',
+            'register'         => 'Register',
+            'errors_occurred'  => 'An error has occurred. Please correct this error.|Errors have occurred, please correct these errors.'
+        ]
 
-];
+    ];
