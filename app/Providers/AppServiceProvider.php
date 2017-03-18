29426f44f4d76852696fd632196e31fffa0f0f48
Max <max@maxkorlaar.com>
2017-03-18 14:34:22 +0100

    Remove idehelper thing

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -28,9 +28,9 @@ class AppServiceProvider extends ServiceProvider
      */
     public function register()
     {
-        if ($this->app->environment() !== 'production') {
+        /*if ($this->app->environment() !== 'production') {
             $this->app->register(\Barryvdh\LaravelIdeHelper\IdeHelperServiceProvider::class);
-        }
+        }*/
         //
     }
 }
