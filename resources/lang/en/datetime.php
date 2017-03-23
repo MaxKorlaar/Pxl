9c6c57b3211325fdca9d063384fcc0e8e1daa87e
Max <max@maxkorlaar.com>
2017-03-23 21:24:11 +0100

    The tiniest of details.

---
+++ resources/lang/en/datetime.php
@@ -0,0 +1,7 @@
+<?php
+
+    return [
+        'format' => [
+            'date_and_time' => 'j/m/Y H:i' // 1/01/2017 12:00
+        ],
+    ];
--- resources/lang/nl/datetime.php
+++ resources/lang/nl/datetime.php
@@ -2,6 +2,6 @@
 
     return [
         'format' => [
-            'date_and_time' => 'j/m/Y H:i' // 1/01/2017 12:00
+            'date_and_time' => 'j-m-Y H:i' // 1/01/2017 12:00
         ],
     ];
