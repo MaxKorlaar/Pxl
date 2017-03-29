0a7aee6c507668aae6b869f8ed807fde4596d8c8
Max <max@maxkorlaar.com>
2017-03-29 20:26:17 +0200

    Add gallery translations

---
+++ resources/lang/en/gallery.php
@@ -0,0 +1,19 @@
+<?php
+
+    return [
+        'image_not_found'            => 'The image cannot be found',
+        'image_deleted'              => 'The image has been deleted successfully',
+        'image_could_not_be_deleted' => 'The image could not be deleted',
+        'title'                      => 'Gallery',
+        'gallery_description'        => 'This is the gallery. If you upload images, you will be able to find them here and delete them if you wish to. Upload one or more images to get started.',
+        'upload'                     => 'Upload',
+        'setup_instructions'         => 'Instructions for setting up uploaders',
+        'no_images'                  => 'No images',
+        'image'                      =>
+            [
+                'copy'                    => 'Copy URL',
+                'delete'                  => 'Delete',
+                'link'                    => 'URL',
+                'scheduled_deletion_time' => 'Scheduled deletion time',
+            ],
+    ];
---
+++ resources/lang/nl/gallery.php
@@ -0,0 +1,19 @@
+<?php
+
+    return [
+        'image_not_found'            => 'De afbeelding kan niet worden gevonden',
+        'image_could_not_be_deleted' => 'De afbeelding kon niet worden verwijderd',
+        'image_deleted'              => 'De afbeelding is successvol verwijderd',
+        'title'                      => 'Galerij',
+        'no_images'                  => 'Geen afbeeldingen',
+        'gallery_description'        => 'Dit is de galerij. Als je afbeeldingen uploadt, kan je deze hier terugvinden en eventueel weer verwijderen. Upload een of meerdere afbeeldingen om te beginnen.',
+        'upload'                     => 'Uploaden',
+        'setup_instructions'         => 'Instructies voor uploaden',
+        'image'                      =>
+            [
+                'link'                    => 'Link',
+                'copy'                    => 'Kopieer link',
+                'delete'                  => 'Verwijderen',
+                'scheduled_deletion_time' => 'Geplande verwijderdatum',
+            ],
+    ];
